977dfb2306475920a1f16b1ed49992ac19bb1ba5
jultsvirbut <jul.tsvirbut@gmail.com>
2016-02-21 18:03:24 +0300

    add app template with header, contacts template

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,12 +6,27 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/gifts/gifts.html',
       controller: 'GiftsController',
       controllerAs: 'gift'
-    })
+      })
     .state('example', {
       url: '/example',
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('app', {
+      abstract: true,
+      url: '',
+      templateUrl: 'app/gifts/app.html'
+    })
+    .state('app.contacts', {
+      url: '/contacts',
+      templateUrl: 'app/gifts/contacts.html'
+    })
+    .state('app.gifts', {
+      url: '/gifts',
+      templateUrl: 'app/gifts/gifts.html',
+      controller: 'GiftsController',
+      controllerAs: 'gift'
     });
 
   $urlRouterProvider.otherwise('/');
